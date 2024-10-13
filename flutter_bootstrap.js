2ab12f32a95f6e9cf22402695e019581ae82e098
biniyam112 <biniyam112@users.noreply.github.com>
2024-10-13 21:54:19 +0000

    deploy: 8dbd865efea26601b0679893c6603927fdb79084

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "362373410"
+    serviceWorkerVersion: "3829906755"
   }
 });
